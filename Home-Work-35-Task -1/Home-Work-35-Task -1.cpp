b450d4b04c31dd6335d17d09c733bc5b83c2c136
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-03-21 20:55:51 +0300

    Добавьте файлы проекта.

---
+++ Home-Work-35-Task -1/Home-Work-35-Task -1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+class Data {
+public:
+    Data(std::initializer_list<int> inData) : data(inData) {}
+
+    void print() {
+        for (auto element : data)
+            std::cout << element << " ";
+    }
+
+private:
+    std::vector<int> data;
+};
+
+int main()
+{
+    Data data = { 1, 2, 3, 4, 5 };
+
+    data.print();
+
+    return 0;
+}
